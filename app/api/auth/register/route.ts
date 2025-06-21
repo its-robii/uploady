57ac63ec64f4c84fc2e3cf10cb9a054f3e1035e3
its-robii <robiulhassanrobi33@gmail.com>
2025-06-21 14:51:46 +0600

    update

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,43 @@
+import { connectToDatabase } from "@/lib/db";
+import User from "@/models/user";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request:NextRequest) {
+    try {
+        const {email, password} = await request.json()
+
+        if(!email || !password) {
+            return NextResponse.json(
+                {error : "email and password is required"},
+                {status : 400}
+            )
+        }
+
+       await connectToDatabase()
+
+       const existingUser = await User.findOne({email})
+
+       if(existingUser) {
+        return NextResponse.json(
+            {error : "user already exist"},
+            {status : 400}
+        )
+       }
+
+      await User.create({
+        email,
+        password
+      })
+
+      return NextResponse.json(
+        {succes : "user is created"},
+        {status : 200}
+      )
+
+    } catch (error) {
+        return NextResponse.json(
+            {error : "server error"},
+            {status : 400}
+        )
+    }
+}
